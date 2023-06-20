6719feb0edbfaed534feed25314aab709c5ef3e1
Moein Arabi <moein.mo81@gmail.com>
2023-06-20 23:01:55 +0330

    Add a new LRU test

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -31,7 +31,7 @@ class MyTestCase(unittest.TestCase):
         fifo(memory, reference_sequence.pop(0))
         self.assertSetEqual(set(memory.frame_list), {4, 3, 2})
 
-    def test_lru(self):
+    def test_lru1(self):
         reference_sequence = [10, 1, 2, 3, 10, 1, 10, 1, 2, 3, 4]
         memory = Memory(3)
 
@@ -57,6 +57,36 @@ class MyTestCase(unittest.TestCase):
         self.assertSetEqual(set(memory.frame_list), {1, 3, 2})
         lru(memory, reference_sequence.pop(0))
         self.assertSetEqual(set(memory.frame_list), {4, 3, 2})
+
+    def test_lru2(self):
+        reference_sequence = [7, 8, 1, 8, 3, 1, 4, 2, 3, 8, 3, 9]
+        memory = Memory(3)
+
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {7})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {7, 8})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {7, 8, 1})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {7, 8, 1})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {1, 8, 3})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {8, 3, 1})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {3, 1, 4})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {1, 4, 2})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {4, 2, 3})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {2, 3, 8})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {2, 8, 3})
+        lru(memory, reference_sequence.pop(0))
+        self.assertSetEqual(set(memory.frame_list), {9, 8, 3})
+
 
 if __name__ == '__main__':
     unittest.main()
