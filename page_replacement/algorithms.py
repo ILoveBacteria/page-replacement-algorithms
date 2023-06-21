7b062ec9a0129908e38bb2414d8dd9f6a06521b2
Moein Arabi <moein.mo81@gmail.com>
2023-06-21 10:27:20 +0330

    Fix second chance algorithm

--- page_replacement/algorithms.py
+++ page_replacement/algorithms.py
@@ -27,19 +27,23 @@ def second_chance(memory: Memory, page):
     if page in map(lambda x: x[0], memory.frame_list):
         memory.page_hits += 1
         # Turn the chance bit true
-        for i, v in enumerate(memory.frame_list):
-            if v[0] == page:
+        for i, (value, bit) in enumerate(memory.frame_list):
+            if value == page:
                 memory.frame_list[i] = (page, 1)
                 return
 
     memory.page_faults += 1
     if not len(memory.frame_list) == memory.frames:
-        memory.frame_list.append((page, 1))
+        memory.frame_list.append((page, 0))
     else:
-        while True:
-            victim, bit = memory.frame_list.pop(0)
-            if bit:
-                memory.frame_list.append((victim, 0))
-                continue
-            memory.frame_list.append((page, 1))
-            break
+        page_replace = False
+        # While there is no page replacement. If all bits are 1
+        while not page_replace:
+            for i, (value, bit) in enumerate(memory.frame_list):
+                if bit:
+                    memory.frame_list[i] = (value, 0)
+                    continue
+                memory.frame_list.pop(i)
+                memory.frame_list.append((page, 0))
+                page_replace = True
+                break
